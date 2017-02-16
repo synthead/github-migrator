66db247a7592035b014ee13dbc97d95aa0ce1b91
Maxwell Pray <synthead@gmail.com>
2017-02-16 01:20:58 -0800

    Create GitHub issues from Bitbucket issues.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'github_api', '~> 0.14'
+gem 'bitbucket_rest_api', '~> 0.1'
---
+++ issue_sources/bitbucket_issue.rb
@@ -0,0 +1,34 @@
+class BitbucketIssue
+  CLOSED_STATUSES = %w(resolved invalid duplicate wontfix).freeze
+
+  def initialize(authentication:, user:, repository:)
+    @bitbucket = BitBucket.new(authentication)
+    @user = user
+    @repository = repository
+  end
+
+  def all
+    all_bitbucket.map { |i| as_github_issue(i) }
+  end
+
+  private
+
+  def all_bitbucket
+    @bitbucket.issues.list_repository(
+      @user,
+      @repository
+    )
+  end
+
+  def as_github_issue(issue)
+    {
+      title: issue['title'],
+      body: issue['content'],
+      closed: issue_closed?(issue)
+    }
+  end
+
+  def issue_closed?(issue)
+    CLOSED_STATUSES.include?(issue['status'])
+  end
+end
