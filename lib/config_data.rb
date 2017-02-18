5ccd183d45d736a63a282791d35d6cb8c23e80e8
Maxwell Pray <synthead@gmail.com>
2017-02-18 02:07:17 -0800

    Add OptionParser support for Bitbucket and GitHub repo locations.

--- lib/config_data.rb
+++ lib/config_data.rb
@@ -1,6 +1,9 @@
 class ConfigData
   def initialize(config_path)
-    @config = load_config_yaml(config_path)
+    @config = %w(github bitbucket).map { |k| [k, {}] }.to_h
+
+    merge_config_yaml_with_exceptions(config_path)
+    merge_options
   end
 
   def [](config_section)
@@ -9,18 +12,38 @@ class ConfigData
 
   private
 
-  def load_config_yaml(config_path)
+  def merge_options
+    option_parser = OptionParser.new do |options|
+      options.on(
+        '--bitbucket-repo REPO',
+        'Bitbucket repository to read issues from'
+      ) do |repo|
+        @config['bitbucket'][:repository] = repo
+      end
+
+      options.on(
+        '--github-repo REPO',
+        'GitHub repository to write issues to'
+      ) do |repo|
+        @config['github'][:repository] = repo
+      end
+    end
+
+    option_parser.parse!(ARGV)
+  end
+
+  def merge_config_yaml(config_path)
     yaml_data = YAML.load_file(config_path)
 
     unless yaml_data.is_a?(Hash)
       abort("Encountered malformed data while reading \"#{path}\"!")
     end
 
-    yaml_data
+    @config.merge!(yaml_data)
   end
 
-  def load_yaml_with_exceptions(path)
-    load_valid_yaml(path)
+  def merge_config_yaml_with_exceptions(path)
+    merge_config_yaml(path)
   rescue Errno::ENOENT
     abort("\"#{path}\" does not exist!")
   rescue Errno::EACCES
